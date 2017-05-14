90c8a1689c80ec7d4ca150c64fb7f0d162f893f1
tiramisu24 <diane.z.zheng@gmail.com>
2017-05-14 07:26:12 -0700

    longest substring

--- numbers.js
+++ numbers.js
@@ -1,16 +1,45 @@
 var addTwoNumbers = function(l1, l2) {
-    let rem, sum, tempNode
-    let newList = new ListNode()
-    while(l1){
-        tempNode = newList
-        sum = l1.val + l2.val + rem
-        rem = (sum) % 10
-        tempNode.val = (sum > 9) ? 1 : sum
+  let sum
+  let rem = 0
+  let newList = new ListNode()
+  let tempNode = newList
 
-        tempNode.next = new ListNode()
-        tempNode = tempNode.next
-        l1 = l1.next
-        l2 = l2.next
+  while(l1 || rem == 1){
+              tempNode.next = new ListNode()
+      tempNode = tempNode.next
+      sum = l1.val + l2.val + rem
+
+      rem =(sum > 9) ? 1 : 0
+
+      tempNode.val = (sum > 9) ? (sum) % 10 : sum
+                              console.log( tempNode.val);
+
+
+      l1 = l1.next
+      l2 = l2.next
+      sum = 0
+  }
+  return newList.next
+};
+
+
+var lengthOfLongestSubstring = function(s) {
+    let repeat = new Set()
+    let arr = []
+    let temp = []
+    for(let i =0; i< s.length; i++){
+        if (repeat.has(s[i])){
+            repeat = new Set()
+            console.log(arr[arr.length -1])
+                        console.log("arr.length")
+                        console.log(arr.length)
+
+            if(arr.length === 0 || (temp.length > arr[arr.length -1].length)) arr.push(temp)
+            temp = []
+        }else {
+            temp.push(s[i])
+            repeat.add(s[i])
+        }
     }
-    return newList
+    return arr[-1].join()
 };
