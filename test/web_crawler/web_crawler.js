359860b0b85570324a32806885a9fcec87e9e25b
tiramisu24 <diane.z.zheng@gmail.com>
2017-06-07 21:17:34 -0700

    skeleton complete

--- test/web_crawler/web_crawler.js
+++ test/web_crawler/web_crawler.js
@@ -2,16 +2,33 @@ const request = require('request');
 const cheerio = require('cheerio');
 const URL = require('url-parse');
 const MAX_PAGES_TO_VISIT = 10;
+let numPagesVisited = 0
 const searchWord = "exchange";
 
 let pageVisited = new Set();
 
-const pageToVisit = "https://www.cnn.com";
-console.log("Visiting page " + pageToVisit);
+const startURL = "https://www.cnn.com";
+const pagesToVisit = [startURL];
+
+const crawl() => {
+  console.log("Visiting page " + startURL);
+  if(numPagesVisited > MAX_PAGES_TO_VISIT){
+    console.log("max pages visited");
+    return;
+  }
+  const page = pagesToVisit.pop();
+  if(pageVisited.has(page)){
+    crawl();
+  }else{
+    visitPage(page, crawl());
+  }
+}
+
+
 const visitPage = (url, cb) => {
   pageVisited.add(url);
 
-  request(pageToVisit, (error, response,body) => {
+  request(startURL, (error, response,body) => {
     if(error){
       console.log("Error: " + error );
       cb();
@@ -20,8 +37,13 @@ const visitPage = (url, cb) => {
 
       if(response.statusCode === 200){
         const content = cheerio.load(body);
-        console.log(searchForWord(content, searchWord));
-        foundLinks(content);
+        if(searchForWord(content, searchWord)){
+          console.log("Word found in " + url);
+          return;
+        }else{
+          foundLinks(content);
+          cb();
+        }
       }
     }
   })
