fb0098808d274f39ca4383abbe342ffbd6058ea7
tiramisu24 <diane.z.zheng@gmail.com>
2017-06-07 21:10:35 -0700

    put request in visit page

--- test/web_crawler/web_crawler.js
+++ test/web_crawler/web_crawler.js
@@ -1,24 +1,31 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const URL = require('url-parse');
+const MAX_PAGES_TO_VISIT = 10;
+const searchWord = "exchange";
+
+let pageVisited = new Set();
 
 const pageToVisit = "https://www.cnn.com";
 console.log("Visiting page " + pageToVisit);
+const visitPage = (url, cb) => {
+  pageVisited.add(url);
 
-request(pageToVisit, (error, response,body) => {
-  if(error){
-    console.log("Error: " + error );
-  }else{
-    console.log("Status code: " + response.statusCode);
+  request(pageToVisit, (error, response,body) => {
+    if(error){
+      console.log("Error: " + error );
+      cb();
+    }else{
+      console.log("Status code: " + response.statusCode);
 
-    if(response.statusCode === 200){
-      const content = cheerio.load(body);
-      console.log(searchForWord(content, "exchange"));
-      foundLinks(content);
+      if(response.statusCode === 200){
+        const content = cheerio.load(body);
+        console.log(searchForWord(content, searchWord));
+        foundLinks(content);
+      }
     }
-  }
-})
-
+  })
+}
 const searchForWord = (content, word) => {
   var bodyText = content('body').text();
   if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
