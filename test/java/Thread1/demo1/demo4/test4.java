0ce0b8fe80cffe98cd6150f4535f129e5ba2dbfa
tiramisu24 <diane.z.zheng@gmail.com>
2017-06-22 16:40:04 -0700

    save demo3

---
+++ test/java/Thread1/demo1/demo4/test4.java
@@ -0,0 +1,36 @@
+package demo4;
+class Runnable extends Thread{
+
+	@Override
+	public void run() {
+		for(int i =0; i<10000;i++){
+			test4.count++;
+		}
+	}
+	
+}
+
+public class test4 {
+	public static int count =0;
+	public static void main(String[] args){
+		test4 t = new test4();
+		t.doWork();
+	}
+	public void doWork(){
+		Runnable r1 = new Runnable();
+		Runnable r2 = new Runnable();
+		
+		r1.start();
+		r2.start();
+		
+		try {
+			r1.join();
+			r2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Count is: " + count);
+		
+	}
+}
