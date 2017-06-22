9ed33f4c4b81186907b12c1745a2f056227c15f6
tiramisu24 <diane.z.zheng@gmail.com>
2017-06-22 13:50:31 -0700

    demo2

--- test/java/Thread1/demo1/demo2/test2.java
+++ test/java/Thread1/demo1/demo2/test2.java
@@ -1,5 +1,31 @@
 package demo2;
 
-public class test2 {
 
+class ThreadDemo1 implements Runnable{
+
+	@Override
+	public void run() {
+		for(int i =0; i<10; i++){
+			System.out.println(i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+public class test2 {
+	public static void main(String[] args){
+		 Thread td1 = new Thread(new ThreadDemo1());
+		 td1.start();
+		 
+		 Thread td2 = new Thread(new ThreadDemo1());
+		 td2.start();
+	}
 }
