9c8b8873dee88f529a5fa47ddee404d1e8623478
tiramisu24 <diane.z.zheng@gmail.com>
2017-07-16 13:00:18 -0700

    add buttons

--- test/dropbox/script.js
+++ test/dropbox/script.js
@@ -3,7 +3,23 @@ console.log("hello");
 $.ajax({
   type: "GET",
   url: "http://localhost:9000/read"
-}).then(d =>
+}).then(d =>{
   data = d;
+  CreateList();
+
+});
+
+const CreateList = () =>{
+  const names = Object.keys(data);
+  const module = $("#module");
+  for(let i = 0; i < names.length; i++){
+    module.append($("<button/>", {
+                    text:names[i],
+                    onClick:clickButton
+                  }))
+  }
+}
+const clickButton = (data) =>{
+  // data.preventDefault();
   console.log(data);
-)
+}
